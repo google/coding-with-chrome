3713021b955f47d0d89e52555b48118132b83445
Markus Bordihn <mbordihn@google.com>
2016-05-09 01:50:37 +0200

    Added additional byte tools functions and unit tests.

--- src/utils/byte_tools.js
+++ src/utils/byte_tools.js
@@ -20,6 +20,14 @@
 goog.provide('cwc.utils.ByteTools');
 
 
+/**
+ * @param {Object} data
+ * @return {number}
+ */
+cwc.utils.ByteTools.bytesToInt = function(data) {
+  return data[0] << 8 | data[1];
+};
+
 
 /**
  * @param {Object} data
@@ -31,9 +39,113 @@ cwc.utils.ByteTools.signedBytesToInt = function(data) {
 
 
 /**
- * @param {Object} data
+ * @param {!Array} data
+ * @return {!Uint8Array}
+ */
+cwc.utils.ByteTools.toUint8Array = function(data) {
+  var dataLength = data.length;
+  var dataBuffer = new Uint8Array(dataLength);
+  for (var i=0; i < dataLength; i++) {
+    dataBuffer[i] = data[i];
+  }
+  return dataBuffer;
+};
+
+
+/**
+ * @param {!Uint8Array} data1
+ * @param {!Uint8Array} data2
+ * @return {!Uint8Array}
+ */
+cwc.utils.ByteTools.joinUint8Array = function(data1, data2) {
+  var data = new Uint8Array(data1.length + data2.length);
+  data.set(data1, 0);
+  data.set(data2, data1.length);
+  return data;
+};
+
+
+/**
+ * @param {ArrayBuffer|Uint8Array} data
+ * @param {Array=} opt_headers
+ * @param {number=} opt_size
+ * @param {ArrayBuffer|Uint8Array} opt_buffer
+ * @return {Uint8Array} result
+ */
+cwc.utils.ByteTools.getUint8Data = function(data,
+    opt_headers, opt_size, opt_buffer) {
+  if (!data) {
+    return null;
+  }
+
+  var dataView = data;
+  if (dataView instanceof ArrayBuffer) {
+    dataView = new Uint8Array(data);
+  }
+
+  // Data processing for data without headers.
+  if (!opt_headers && !opt_size) {
+    return dataView;
+  }
+
+  // Additional length checks if needed.
+  if (opt_size) {
+
+    // Prepend buffer if needed.
+    if (opt_buffer && dataView.length < opt_size) {
+      var buffer = opt_buffer;
+      if (opt_buffer instanceof ArrayBuffer) {
+        buffer =  new Uint8Array(opt_buffer);
+      }
+      dataView = cwc.utils.ByteTools.joinUint8Array(buffer, dataView);
+    }
+
+    if (dataView.length < opt_size) {
+      return null;
+    }
+  }
+
+  // Data processing for data with headers.
+  if (opt_headers) {
+    var headerPosition = cwc.utils.ByteTools.getHeaderPosition(dataView,
+        opt_headers);
+    if (headerPosition === null) {
+      return null;
+    }
+
+    if (headerPosition !== 0) {
+      dataView = dataView.slice(headerPosition);
+    }
+  }
+
+  return dataView;
+};
+
+
+/**
+ * @param {Uint8Array|Array} data
+ * @param {number|Array} headers
  * @return {number}
  */
-cwc.utils.ByteTools.bytesToInt = function(data) {
-  return data[0] << 8 | data[1];
+cwc.utils.ByteTools.getHeaderPosition = function(data, headers) {
+  var headerPosition = null;
+  var dataLength = data.length;
+  if (!data || dataLength < headers.length) {
+    return headerPosition;
+  }
+
+  if (headers instanceof Array) {
+    var searchPosition = 0;
+    while (searchPosition !== -1 && searchPosition < dataLength) {
+      searchPosition = data.indexOf(headers[0], searchPosition);
+      if (data[searchPosition + 1] === headers[1]) {
+        headerPosition = searchPosition;
+        break;
+      }
+      searchPosition++;
+    }
+  } else if (data.indexOf(headers) !== -1) {
+    headerPosition = data.indexOf(headers);
+  }
+  return headerPosition;
 };
---
+++ test/unit_tests/utils/byte_tools_test.js
@@ -0,0 +1,114 @@
+/**
+ * @fileoverview ByteTools tests.
+ *
+ * @license Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author mbordihn@google.com (Markus Bordihn)
+ */
+goog.require('cwc.utils.ByteTools');
+
+
+describe('ByteTools', function() {
+
+  it('bytesToInt', function() {
+    expect(cwc.utils.ByteTools.bytesToInt([0x00000000, 0x00000001]))
+      .toEqual(1);
+    expect(cwc.utils.ByteTools.bytesToInt([0x00000001, 0x00000000]))
+      .toEqual(256);
+    expect(cwc.utils.ByteTools.bytesToInt([0x00000001, 0x00000001]))
+      .toEqual(257);
+    expect(cwc.utils.ByteTools.bytesToInt([0x00000011, 0x00000001]))
+      .toEqual(4353);
+    expect(cwc.utils.ByteTools.bytesToInt([0x10101010, 0x10101010]))
+      .toEqual(269488144);
+  });
+
+  it('signedBytesToInt', function() {
+    expect(cwc.utils.ByteTools.signedBytesToInt([0xF0F0F0F0, 0xF0F0F0F0]))
+        .toBe(-3856);
+    expect(cwc.utils.ByteTools.signedBytesToInt([0xFFFFFFFF, 0x00000000]))
+        .toBe(-256);
+    expect(cwc.utils.ByteTools.signedBytesToInt([0xFFFFFFFF, 0xFFFFFFFF]))
+        .toBe(-1);
+    expect(cwc.utils.ByteTools.signedBytesToInt([0x00000000, 0x00000001]))
+        .toBe(1);
+    expect(cwc.utils.ByteTools.signedBytesToInt([0x00000001, 0x00000000]))
+        .toBe(256);
+    expect(cwc.utils.ByteTools.signedBytesToInt([0x00000001, 0x00000001]))
+        .toBe(257);
+    expect(cwc.utils.ByteTools.signedBytesToInt([0x00000011, 0x00000001]))
+        .toBe(4353);
+    expect(cwc.utils.ByteTools.signedBytesToInt([0x10101010, 0x10101010]))
+        .toBe(4112);
+  });
+
+  it('joinUint8Array', function() {
+    var data1 = cwc.utils.ByteTools.toUint8Array([0x10101010]);
+    var data2 = cwc.utils.ByteTools.toUint8Array([0x10101010]);
+    var result = cwc.utils.ByteTools.toUint8Array([0x10101010, 0x10101010]);
+    expect(cwc.utils.ByteTools.joinUint8Array(data1, data2))
+        .toEqual(result);
+  });
+
+  it('getUint8Data', function() {
+    var packet1 = cwc.utils.ByteTools.toUint8Array(
+        [255, 255, 0, 21, 4, 255, 0, 0, 231]);
+    var packet1_shifted = cwc.utils.ByteTools.toUint8Array(
+        [0, 255, 0, 255, 255, 0, 21, 4, 255, 0, 0, 231]);
+    var packet1_broken = cwc.utils.ByteTools.toUint8Array(
+        [255, 0, 21, 4, 255, 0, 0, 231]);
+    var packet2 = cwc.utils.ByteTools.toUint8Array(
+        [255, 255, 0, 21, 4, 0, 255, 0, 231]);
+    var packet3 = cwc.utils.ByteTools.toUint8Array(
+        [255, 255, 0, 21, 4, 0, 0, 255, 231]);
+    var packet4 = cwc.utils.ByteTools.toUint8Array(
+        [255, 255, 0, 16, 11, 255, 250, 255, 252, 0, 0, 0, 0, 0, 0, 240]);
+    var packet5 = cwc.utils.ByteTools.toUint8Array(
+        [255, 255, 0, 16, 11, 255, 253, 255, 250, 0, 24, 255, 247, 0, 25, 200]);
+    var packet6 = cwc.utils.ByteTools.toUint8Array(
+        [255, 255, 0, 16, 11, 0, 4, 255, 235, 0, 0, 0, 0, 0, 0, 246]);
+    var headers1 = [0xff, 0xff];
+    /*var headers2 = [0xff, 0xfe];*/
+    var size1 = 9;
+    var size2 = 16;
+    var buffer1 = cwc.utils.ByteTools.toUint8Array([0, 255]);
+    expect(cwc.utils.ByteTools.getUint8Data(packet1, headers1, size1))
+        .toEqual(packet1);
+    /*expect(cwc.utils.ByteTools.getUint8Data(packet1, headers2, size1))
+        .toEqual(null);*/
+    expect(cwc.utils.ByteTools.getUint8Data(packet1_shifted, headers1, size1))
+        .toEqual(packet1);
+    expect(cwc.utils.ByteTools.getUint8Data(packet1_shifted, headers1, size1,
+        buffer1))
+        .toEqual(packet1);
+    expect(cwc.utils.ByteTools.getUint8Data(packet1_broken, headers1, size1))
+        .toEqual(null);
+    expect(cwc.utils.ByteTools.getUint8Data(packet1_broken, headers1, size1,
+        buffer1))
+        .toEqual(packet1);
+    expect(cwc.utils.ByteTools.getUint8Data(packet2, headers1, size1))
+        .toEqual(packet2);
+    expect(cwc.utils.ByteTools.getUint8Data(packet3, headers1, size1))
+        .toEqual(packet3);
+    expect(cwc.utils.ByteTools.getUint8Data(packet4, headers1, size2))
+        .toEqual(packet4);
+    expect(cwc.utils.ByteTools.getUint8Data(packet5, headers1, size2))
+        .toEqual(packet5);
+    expect(cwc.utils.ByteTools.getUint8Data(packet6, headers1, size2))
+        .toEqual(packet6);
+  });
+
+
+});
