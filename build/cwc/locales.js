3abb08ef2c0af2655b90fc2c346174e342f465d6
Markus Bordihn <mbordihn@google.com>
2017-03-23 18:13:39 +0100

    Fixed translation file.

---
+++ build/cwc/locales.js
@@ -0,0 +1,35 @@
+/**
+ * @fileoverview BUILD configuration for Coding with Chrome locales files.
+ *
+ * @license Copyright 2017 The Coding with Chrome Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author mbordihn@google.com (Markus Bordihn)
+ */
+var closureBuilder = require('closure-builder');
+var glob = closureBuilder.globSupport();
+
+
+
+/**
+ * UI Builder
+ */
+closureBuilder.build({
+  name: 'cwc.locales.de.Translation',
+  srcs: glob([
+    'src/locales/de/translation.js',
+  ]),
+  compress: true,
+  out: 'genfiles/js/locales/de.js'
+});
