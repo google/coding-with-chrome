5594d7e7d1e362394100def5893da24c301ec94b
Chan <chenzhiyu512@gmail.com>
2018-11-08 09:17:51 +0800

    Added Chinese Translation

---
+++ locales/zho/status.js
@@ -0,0 +1,39 @@
+/**
+ * @fileoverview Translation file for the Status bar / button (chinese).
+ *
+ * @license Copyright 2018 The Coding with Chrome Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author chenzhiyu512@gmail.com (Chan)
+ */
+Locales['zho']['STATUS'] = {
+  'LOADED': '{$seconds} 秒后完成.',
+  'LOADING': '载入中 ...',
+  'PREPARE': '准备',
+  'RELOADING': '重新载入中 ...',
+  'RUNNING': '运行中 ...',
+  'STOPPED': '停止',
+  'TERMINATED': 'Terminated',
+  'UNRESPONSIVE': '未响应',
+};
+
+Locales['zho']['STATUS_BUTTON'] = {
+  'COLLAPSE_VIEW': 'Collapse view',
+  'EXPAND_VIEW': 'Expand view',
+  'OPEN_IN_BROWSER': 'Open view in new Browser window',
+  'REFRESH_VIEW': 'Refresh view',
+  'RUN_CODE': 'Runs the code and updates the view',
+  'STOP_VIEW': 'Stops or terminates the view',
+  'TERMINATE_VIEW': 'Terminate view',
+};
---
+++ locales/zho/welcome_screen.js
@@ -0,0 +1,34 @@
+/**
+ * @fileoverview Translation file for the Welcome screen (chinese).
+ *
+ * @license Copyright 2018 The Coding with Chrome Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author chenzhiyu512@gmail.com (Chan)
+ */
+/* eslint-disable max-len */
+Locales['zho']['WELCOME_SCREEN'] = {
+  'TITLE': '欢迎使用 Coding with Chrome!',
+  'DESCRIPTION': 'Coding with Chrome 适用于编程教学',
+  'HEADER': '开始编程 ...',
+  'CHOOSE': '首先请选择您目前的编程水平',
+  'BEGINNER': '初学者',
+  'BEGINNER_TEXT': 'Begin to learn coding with visual block programming. Put together the program steps by dragging and dropping coding blocks.',
+  'BEGINNER_ACTION': '使用初学模式',
+  'ADVANCED': '有一定基础的学生',
+  'ADVANCED_TEXT': ' Use a high-level programming language like JavaScript or Python to write your code',
+  'ADVANCED_ACTION': '使用进阶模式',
+  'SHOW_ON_STARTUP': '每次启动时显示',
+  'SHOW_ON_STARTUP_NOTE': '您可以在设置中修改',
+};
