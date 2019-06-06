3c558e290071c6442b6cbc777373b8efcc966c2a
Salikh Zakirov <salikh@google.com>
2019-06-07 02:34:45 +0900

    Add a few Japanese translations for SELECT_SCREEN

---
+++ locales/jpn/select_screen.js
@@ -0,0 +1,40 @@
+/**
+ * @fileoverview Translation file for the Select screen (Japanese).
+ *
+ * @license Copyright 2018 The Coding with Chrome Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author salikh@google.com (Salikh Zakirov)
+ */
+Locales['jpn']['SELECT_SCREEN'] = {
+  'BLUETOOTH_NOT_AVAILABLE': 'ブルーツースには対応していない',
+  'BLUETOOTH_WEB_LIGHT': 'Windows OSではブルーツースウェブ機能は不十分だ',
+  'BLUETOOTH_WEB_NOT_AVAILABLE': 'ブルーツースウェブ機能は不十分だ',
+  'LOAD_FILE': '{$title}をロードする',
+  'USE': '使う',
+};
+
+Locales['jpn']['SELECT_SCREEN_NORMAL'] = {
+  'TITLE': 'ブロックでコード',
+  'DESCRIPTION': 'ブロックによるプログラミング',
+  'BLOCKLY': 'Blockly',
+  'BLOCKLY_ACTION': 'Blocklyを起動',
+  'BLOCKLY_TEXT': 'Blocklyでブロックをドラッグ・アンド・ドロップしながらコードを作る',
+  'GAME': 'ゲーム',
+  'GAME_ACTION': 'ゲームを作り始める',
+  'GAME_TEXT': 'ブロックをドラッグ・アンド・ドロップしながらゲームを作る',
+  'ROBOTS': 'ロボット',
+  'ROBOTS_ACTION': 'ロボットの制御',
+  'ROBOTS_TEXT': '仮想または実際のロボットをBlocklyで制御する',
+};
