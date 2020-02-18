cb4895046f0466e3915dbb71b7857fb20ac1dd9d
Shivay Lamba <shivaylamba@gmail.com>
2020-02-18 17:26:01 +0530

    Added Welcome Screen Translation for Hindi

---
+++ locales/hin/welcome_screen.js
@@ -0,0 +1,35 @@
+/**
+ * @fileoverview Translation file for the Welcome screen (Hindi).
+ *
+ * @license Copyright 2018 The Coding with Chrome Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author shivaylamba@gmail.com (Shivay Lamba)
+ */
+/* eslint-disable max-len */
+Locales['hin']['WELCOME_SCREEN'] = {
+    'TITLE': 'क्रोम के साथ कोडिंग में आपका स्वागत है !',
+    'DESCRIPTION': 'क्रोम के साथ कोडिंग एक शैक्षिक विकास वातावरण है',
+    'HEADER': 'अब कोडिंग शुरू करें...',
+    'CHOOSE': 'सबसे पहले, कृपया अपना वर्तमान कोडिंग कौशल चुने',
+    'BEGINNER': 'नौसिखिया',
+    'BEGINNER_TEXT': 'दृश्य ब्लॉक प्रोग्रामिंग के साथ कोडिंग सीखना शुरू करें। कोडिंग ब्लॉक को ड्रैग और ड्रॉप करके प्रोग्राम स्टेप्स को एक साथ रखें।',
+    'BEGINNER_ACTION': 'शुरुआत मोड का उपयोग करें',
+    'ADVANCED': 'उन्नत',
+    'ADVANCED_TEXT': 'अपना कोड लिखने के लिए जावास्क्रिप्ट या पायथन जैसी उच्च स्तरीय प्रोग्रामिंग भाषा का उपयोग करें',
+    'ADVANCED_ACTION': 'उन्नत मोड का उपयोग करें',
+    'SHOW_ON_STARTUP': 'स्टार्टअप पर स्क्रीन दिखाएं',
+    'SHOW_ON_STARTUP_NOTE': 'आप सेटिंग मेनू में किसी भी समय अपनी पसंद बदल सकते हैं',
+  };
+  
