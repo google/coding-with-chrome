9ece82d84f993e71689faa6569d8d3312ae21a9b
Markus Bordihn <mbordihn@google.com>
2021-06-10 12:56:24 +0200

    Updated packages and added npm v7.x as dependency.

--- build/webpack.config.test.babel.js
+++ build/webpack.config.test.babel.js
@@ -30,10 +30,7 @@ testConfig.plugins = testConfig.plugins
   .filter((p) => !(p instanceof WebpackPwaManifest));
 testConfig.module.rules.push({
   test: /\.js$|\.jsx$/,
-  use: {
-    loader: '@jsdevtools/coverage-istanbul-loader',
-    options: { esModules: true },
-  },
+  use: ['@jsdevtools/coverage-istanbul-loader', 'babel-loader'],
   enforce: 'pre',
   exclude: /node_modules|_test\.js$/,
 });
